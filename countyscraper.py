32e008d9f5d1d34b4e565dc1aa3138c1cdc8f7bb
Josh <QuestofIranon@users.noreply.github.com>
2015-07-04 17:03:15 -0500

    It should hypothetically work, only tested on 20 urls though

--- countyscraper.py
+++ countyscraper.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import json
-import pprint
 import requests
 
 def ExtractJSONAPNS(fileloc):
@@ -12,6 +11,8 @@ def ExtractJSONAPNS(fileloc):
 
 	apndict = dict()
 
+	apncount = 0
+
 	for key in extractedJSON["features"]:
 		properties = key['properties']
 
@@ -20,17 +21,31 @@ def ExtractJSONAPNS(fileloc):
 
 		if len(apn) is 17:
 			apndict[parcel] = '{0}-{1}-{2}-{3}-{4}-{5}-{6}-{7}'.format(apn[0:2], apn[2:5], apn[5:7], apn[7:9], apn[9:11], apn[11], apn[12:14], apn[14:len(apn)])
-			print apndict[parcel]
+			apncount += 1
+
 	extractedJSON.clear()
+	print "Total APNS Found: " + str(apncount)
 	return apndict
 
 def ExtractHTMLTree(URL):
 	page = requests.get(URL)
 	return BeautifulSoup(page.text, 'lxml')
 
+def saveToFiles(parcels):
+
+	jsonout = json.JSONEncoder().encode(parcels)
+
+	print "Writing to file: ParcelData.json"
+	file = open('ParcelData.json', 'w')
+	file.write(JSONString)
+	file.close()
+
 #######TAX INFORMATION#######
 def ScrapeTaxInfo(html):
 	table = html.find("table", attrs={"id":"mTabGroup_Values_mValues_mGrid_RealDataGrid"})
+
+	if len(table) == 0:
+		return None
 
 	rows = table.find_all("tr")
 
@@ -48,7 +63,7 @@ def ScrapeTaxInfo(html):
 		values[str(taxablevalues[0]).strip()] = int(taxablevalues[count].replace(',', ''))
 		values[str(assessvalues[0]).strip()] = int(assessvalues[count].replace(',', ''))
 		yeardict[str(year)] = values
-		++count
+		count += 1
 
 	return yeardict
 
@@ -103,40 +118,30 @@ def ScrapeIncentives(html):
 	return incentivesDict
 
 
-
 def main():
 	baseURL = "http://maps.jacksongov.org/PropertyReport/PropertyReport.cfm?pid="
 
 	apns = ExtractJSONAPNS("wendellphillipszonesjoin.geojson")
 
-
-	parcellist = []
-
-	count = 0
+	parceldict = dict()
 
 	for key, value in apns.iteritems():
-		if count is 10:
-			break
 
 		parcelinfo = dict()
 		html = ExtractHTMLTree(baseURL + value)
-		print(baseURL + value)	
-		parcelinfo["APN"] = key
+
 		parcelinfo["Property Values"] = ScrapeTaxInfo(html)
-		parcelinfo["Exemptions"] = ScrapeExemptions(html)
-		parcelinfo["Incentives"] = ScrapeIncentives(html)
+		if parcelinfo["Property Values"] is None:
+			parcelinfo["Exemptions"] = None
+			parcelinfo["Incentives"] = None
+		else:
+			parcelinfo["Exemptions"] = ScrapeExemptions(html)
+			parcelinfo["Incentives"] = ScrapeIncentives(html)
 
-		parcellist.append(parcelinfo)
-		++count
+		parceldict[key] = parcelinfo
 
-	jsonout = json.JSONEncoder().encode(parcellist)
-
-	pprint.pprint(jsonout)
+	saveToFiles(parceldict)
 
 
 if __name__ == "__main__":
 	main()
-
-
-
-#print jsonout
