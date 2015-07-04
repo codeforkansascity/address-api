e46c863d85b2abc9f7f8b96f3b14a422f9b7ecfd
Josh <QuestofIranon@users.noreply.github.com>
2015-07-04 12:02:05 -0500

    Now gathers Incentives and stores info as a json

--- countyscraper.py
+++ countyscraper.py
@@ -3,9 +3,11 @@ import json
 import requests
 
 extractionJSON = "/wendellphillipszonesjoin.geojson"
-page = requests.get('http://maps.jacksongov.org/PropertyReport/propertyreport.cfm?pid=28-620-08-02-00-0-00-000')
+page = requests.get('http://maps.jacksongov.org/PropertyReport/PropertyReport.cfm?pid=29-220-20-01-00-0-00-000')
 
 soup = BeautifulSoup(page.text, 'lxml')
+
+parcelinfo = dict()
 
 #######TAX INFORMATION#######
 table = soup.find("table", attrs={"id":"mTabGroup_Values_mValues_mGrid_RealDataGrid"})
@@ -17,15 +19,13 @@ marketvalues = [td.get_text() for td in rows[1].find_all("td")]
 taxablevalues = [td.get_text() for td in rows[2].find_all("td")]
 assessvalues = [td.get_text() for td in rows[3].find_all("td")]
 
-taxyears = dict()
-
 count = 1
 for year in years[1:len(years)]:
 	values = dict()
-	values[marketvalues[0]] = marketvalues[count]
-	values[taxablevalues[0]] = taxablevalues[count]
-	values[assessvalues[0]] = assessvalues[count]
-	taxyears[year] = values
+	values[str(marketvalues[0]).strip()] = int(marketvalues[count].replace(',', ''))
+	values[str(taxablevalues[0]).strip()] = int(taxablevalues[count].replace(',', ''))
+	values[str(assessvalues[0]).strip()] = int(assessvalues[count].replace(',', ''))
+	parcelinfo[str(year)] = values
 	++count
 
 ########EXEMPTION INFORMATION######
@@ -33,13 +33,42 @@ table = soup.find("table", attrs={"id":"mTabGroup_Exemptions_mActiveExemptions_m
 
 rows = table.find_all("tr")
 
-exemptions = [None]
+exemptions = []
 
 if len(rows) > 0:
 
 	for row in rows:
-		exemptions.append(row.get_text().strip())
+		print row
+		exemptions.append(str(row.get_text().strip()))
 
 
-##########Incentive information#######
+parcelinfo["Exemptions"] = exemptions
 
+
+##########INCENTIVE INFORMATION#######
+lx = soup.find_all("span", attrs={"style":"margin-left:20px;"})
+
+lm = lx[1].find_all("span")
+
+firstelem = True
+
+for element in lm[0:6]:
+	key = ""
+	value = "null"
+	if firstelem:
+		key = "Capital Improvement Project"
+		firstelem = False
+
+	for a in element:
+		if str(type(a)) == "<class 'bs4.element.NavigableString'>":
+			a = a.strip()
+			if a != "":
+				value = str(a)
+		elif str(type(a)) == "<class 'bs4.element.Tag'>":
+			key = str(a.get_text()).strip().strip(':')
+
+	parcelinfo[key] = value
+
+jsonout = json.JSONEncoder().encode(parcelinfo)
+
+print jsonout
