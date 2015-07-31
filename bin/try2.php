485da32fcfa783761cf777f8ead17976fa6390d8
Paul D Barham <paulb@savagesoft.com>
2015-07-30 22:23:11 -0500

    Can get data from census geocoder

---
+++ bin/try2.php
@@ -0,0 +1,13 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use \Httpful\Request;
+
+$uri = "http://geocoding.geo.census.gov/geocoder/geographies/address?street=210+w+19th+terrace&city=kansas+city&state=mo&zip=&benchmark=4&vintage=4&format=json";
+$response = Request::get($uri)->send();
+
+print_r($response);
+
+
+
