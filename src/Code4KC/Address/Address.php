3ea0f0a6721947ae42f86066ce2902c6bc4d1418
Paul Barham <paulb@savagesoft.com>
2015-08-23 23:56:56 +0000

    inital address lookup

--- src/Code4KC/Address/Address.php
+++ src/Code4KC/Address/Address.php
@@ -13,6 +13,7 @@ class Address extends BaseTable
     var $table_name = 'address';
     var $primary_key_sequence = 'address_id_seq_02';
     var $single_line_address_query = '';
+    var $get_attributes_query = '';
     var $fields = array(
         'single_line_address' => '',
         'street_number' => '',
@@ -51,6 +52,47 @@ class Address extends BaseTable
         return $this->single_line_address_query->fetch(PDO::FETCH_ASSOC);
     }
 
+    function get_attributes( $address_id )
+    {
 
+        if (!$this->get_attributes_query) {
+            $sql = 'SELECT 
+c.land_use_code AS city_land_use_code,
+c.land_use AS city_land_use,
+c.classification AS city_classification,
+c.sub_class AS city_sub_class,
+c.neighborhood AS city_nighborhood,
+
+b.block_2010_name AS census_block_2010_name,
+b.block_2010_id AS census_block_2010_id,
+b.tract_name AS census_track_name,
+b.tract_id AS census_track_id,
+b.zip AS census_zip,
+b.county_id AS census_county_id,
+b.state_id AS census_county_state_id,
+b.longitude AS census_longitude,
+b.latitude AS census_latitude,
+b.tiger_line_id AS census_tiger_line_id
+
+
+FROM address_keys k 
+LEFT JOIN census_attributes b ON b.city_address_id = k.city_address_id
+LEFT JOIN city_address_attributes c ON c.id = k.city_address_id
+LEFT JOIN county_address_attributes j ON j.id = k.county_address_id
+WHERE k.address_id = :address_id';
+            $this->get_attributes_query = $this->dbh->prepare("$sql  -- " . __FILE__ . ' ' . __LINE__);
+        }
+
+        try {
+            $this->get_attributes_query->execute(array(':address_id' => $address_id));
+        } catch (PDOException  $e) {
+            error_log($e->getMessage() . ' ' . __FILE__ . ' ' . __LINE__);
+            //throw new Exception('Unable to query database');
+            return false;
+        }
+
+        return $this->get_attributes_query->fetch(PDO::FETCH_ASSOC);
+
+    }
     
 }
--- webroot/index.php
+++ webroot/index.php
@@ -1,12 +1,84 @@
 <?php
-
+// http://api.codeforkc.org/normalize_address/v0000/210%20West%2019th%20terrace/?city=KANSAS%20CITY&state=MO
 
 require '../vendor/autoload.php';
+require '../config/config.php';
 
+require '../vendor/Convissor/address/AddressStandardizationSolution.php';
 
 $app = new \Slim\Slim();
 
 
+$app->get('/normalize_address/v0000/:address/', function ($id) use ($app) {
+
+    list( $in_address, $x ) = explode("?",$id);
+
+    $in_city = $app->request()->params('city');
+    $in_state = $app->request()->params('state');
+
+global $DB_NAME;
+global $DB_USER;
+global $DB_PASS;
+global $DB_HOST;
+
+    try {
+        $dbh = new PDO("pgsql:dbname=$DB_NAME", $DB_USER, $DB_PASS);
+    } catch (PDOException $e) {
+        error_log($e->getMessage() . ' ' . __FILE__ . ' ' . __LINE__);
+        throw new Exception('Unable to connect to database');
+    }
+
+    $address_converter = new Convissor\address\AddressStandardizationSolution();
+    $address = new \Code4KC\Address\Address($dbh, true);
+    $address_alias = new \Code4KC\Address\AddressAlias($dbh, true);
+    $address_keys = new \Code4KC\Address\AddressKeys($dbh, true);
+    $city_address_attributes = new \Code4KC\Address\CityAddressAttributes($dbh, true);
+
+
+    $single_line_address  = $address_converter->AddressLineStandardization($in_address);
+
+    $single_line_address .= ', ' . strtoupper($in_city) . ', ' . strtoupper($in_state);           // We keep unit 'internal'
+
+    $census = new \Code4KC\Address\Census();
+    $normalized_address = $census->normalize_address($single_line_address);                // Strips off unit 'internal' 
+
+    if ( $exisiting_address_alias_rec = $address_alias->find_by_single_line_address( $single_line_address ) ) {
+
+print "<h1>hit</h1><pre>";
+    print_r($normalized_address);
+    print_r($exisiting_address_alias_rec);
+print "</pre>";
+
+        $address_id = $exisiting_address_alias_rec['address_id'];
+        if ( $address_rec = $address->find_by_id( $address_id ) ) {
+
+print "<pre>";
+print_r($address_rec);
+print "</pre>";
+
+            if ( $attributes = $address->get_attributes( $address_id ) ) {
+
+print "<pre>";
+print_r($attributes);
+print "</pre>";
+
+                $rec = array_merge($address_rec, $attributes);
+
+print "<pre>";
+print_r($rec);
+print "</pre>";
+
+            }
+        }
+
+    } else {
+echo "MISS";
+    }
+    print_r($normalized_address);
+
+echo "END";
+
+});
 $app->get('/jd_wp/(:id)', function ($id) use ($app) {
 
   $row = array('not init');
