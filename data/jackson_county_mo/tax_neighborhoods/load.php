c678fa991998ae2cf5ed8eead0c4de6995412564
Paul Barham <paulb@savagesoft.com>
2015-09-27 23:44:20 +0000

    Addes records

--- data/jackson_county_mo/tax_neighborhoods/load.php
+++ data/jackson_county_mo/tax_neighborhoods/load.php
@@ -95,7 +95,8 @@ $totals = array(
     'address' => array('insert' => 0, 'update' => 0, 'N/A' => 0, 'error' => 0),
     'address_alias' => array('insert' => 0, 'update' => 0, 'N/A' => 0, 'error' => 0),
     'address_keys' => array('insert' => 0, 'update' => 0, 'N/A' => 0, 'error' => 0),
-    'city_address_attributes' => array('insert' => 0, 'update' => 0, 'N/A' => 0, 'error' => 0),
+    'county_address_attributes' => array('insert' => 0, 'update' => 0, 'N/A' => 0, 'error' => 0),
+    'county_address_data' => array('insert' => 0, 'update' => 0, 'N/A' => 0, 'error' => 0),
 );
 
 try {
@@ -117,7 +118,8 @@ $address_converter = new Convissor\address\AddressStandardizationSolution();
 $address = new \Code4KC\Address\Address($dbh, true);
 $address_alias = new \Code4KC\Address\AddressAlias($dbh, true);
 $address_keys = new \Code4KC\Address\AddressKeys($dbh, true);
-$city_address_attributes = new \Code4KC\Address\CityAddressAttributes($dbh, true);
+$county_address_attributes = new \Code4KC\Address\CountyAddressAttributes($dbh, true);
+$county_address_data = new \Code4KC\Address\CountyAddressData($dbh, true);
 
 
 $sql = "SELECT * FROM address_spatial.jackson_county_mo_tax_neighborhoods WHERE situs_city = 'KANSAS CITY' LIMIT 20 ";
@@ -137,8 +139,44 @@ while ($data = $query->fetch(PDO::FETCH_ASSOC)) {
     $row++;
     $parcel_number = $data['parcel_number'];
     $city_parcel_number = convert2city_parcel_number($parcel_number);
-    print "$parcel_number $city_parcel_number\n";
-    
+
+
+            $single_line_address = $address_converter->AddressLineStandardization($data['situs_address']);
+
+            $single_line_address .= ', ' . strtoupper($data['situs_city']) . ', ' . strtoupper($data['situs_state']);                    // We keep unit 'internal'
+                $normalized_address = $census->normalize_address($single_line_address);                // Strips off unit 'internal'
+
+    print "$parcel_number $city_parcel_number $single_line_address\n";
+
+            $data['id'] = $city_parcel_number;                      // FIX THIS IN V2
+
+            $address_id = 0;
+            if ($exisiting_address_alias_rec = $address_alias->find_by_single_line_address($single_line_address)) {
+                $totals['address_alias']['N/A']++;
+                $address_id = $exisiting_address_alias_rec['address_id'];
+
+
+               if ($address_key_rec = $address_keys->find_by_county_address_id($city_parcel_number)) {
+                    print_r($address_key_rec);
+                    if ($county_address_rec = $county_address_attributes->find_by_id($city_parcel_number)) {
+                    $totals['county_address_attributes']['N/A']++;
+                    } else {
+                    $county_address_attributes->add($data);
+                    $totals['county_address_attributes']['insert']++;
+                    }
+
+                    if ($county_address_rec = $county_address_data->find_by_id($city_parcel_number)) {
+                    $totals['county_address_data']['N/A']++;
+                    } else {
+                    $county_address_data->add($data);
+                    $totals['county_address_data']['insert']++;
+                    }
+                } else {
+
+                }
+            } else {
+
+            }    
 }
 
 print "\nTotals\n--------------------------------------------------------------------------\n";
